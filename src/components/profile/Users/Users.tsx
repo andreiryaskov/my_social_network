c8ab99cfbecf1c1f9808f6a0e327eb17eeec372c
andreiryaskov <andreiryaskov@gmail.com>
2022-06-09 12:17:52 +0300

    users_page

--- src/components/profile/Users/Users.tsx
+++ src/components/profile/Users/Users.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import s from './Users.module.scss';
+import Paper from "@mui/material/Paper";
+import {AppRoutes} from "../../../Routes";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../../redux/store";
+import {UsersInitialStateType, UsersType} from "../../../redux/reducers/users-reducer";
 
 const Users = () => {
+
+    const users = useSelector<AppRootStateType, UsersType[]>(state => state.users.users)
+
     return (
-        <div>
-            Users
-        </div>
+
+        <Grid direction={"column"}>
+            <Paper className={s.users_wrapper}>
+            {
+                users.map(u => {
+                    return (
+
+                            <Paper className={s.user_container}>
+                                <Grid container spacing={2} padding={"20px"}>
+                                    <Grid item xs={4}>
+                                        <div className={s.user_avatar}>
+                                            <img src={u.photoUrl} alt="avatar"/>
+                                        </div>
+                                        <Button>
+                                            {u.followed
+                                            ? 'Unfollow'
+                                            : 'Follow'}
+                                        </Button>
+                                    </Grid>
+                                    <Grid item xs={8}>
+                                        <Grid container spacing={2}
+                                              justifyContent={"space-between"}>
+                                            <Grid item xs={6}
+                                                  justifyContent={"space-between"}>
+                                                <div>
+                                                    {u.name}
+                                                </div>
+                                                <div>
+                                                    {u.status}
+                                                </div>
+                                            </Grid>
+                                            <Grid item xs={6} textAlign={"right"}>
+                                                <div>
+                                                    {u.location.city}
+                                                </div>
+                                                <div>
+                                                    {u.location.country}
+                                                </div>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+
+                    )
+                })
+            }
+            </Paper>
+        </Grid>
     );
 };
 
--- src/redux/reducers/users-reducer.ts
+++ src/redux/reducers/users-reducer.ts
@@ -1,11 +1,58 @@
+import {v1} from "uuid";
+
 export const initialState:UsersInitialStateType = {
-    name: '',
-    id: ''
+    users: [
+        {
+            name: 'andrei',
+            id: v1(),
+            photoUrl: 'https://icdn.lenta.ru/images/2018/05/30/22/20180530221332205/wide_7fe601a38687939f2f7f9dc906b53d99.jpg',
+            followed: true,
+            status: 'i am a frontend developer, bich',
+            location: { city: 'Delaver', country: 'USA'}
+        },
+        {
+            name: 'vadim kozyulin',
+            id: v1(),
+            photoUrl: 'https://sgia.mgimo.ru/upload/images/article_img/6036a194ca099.jpg',
+            followed: true,
+            status: 'i am a frontend developer, bich',
+            location: { city: 'Moscow', country: 'Russia'}
+        },
+        {
+            name: 'nika',
+            id: v1(),
+            photoUrl: 'https://images.theconversation.com/files/22197/original/vbmrmrkq-1365429883.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop',
+            followed: true,
+            status: 'what is "Tatcherism"?',
+            location: { city: 'London', country: 'GB'}
+        },
+        {
+            name: 'john',
+            id: v1(),
+            photoUrl: 'https://cdn.profoto.com/cdn/053149e/contentassets/d39349344d004f9b8963df1551f24bf4/profoto-albert-watson-steve-jobs-pinned-image-original.jpg?width=1280&quality=75&format=jpg',
+            followed: false,
+            status: 'the story behind the image',
+            location: { city: 'silicon valley', country: 'USA'}
+        }
+    ]
 }
 
 export type UsersInitialStateType = {
+    [key:string]: UsersType[]
+}
+
+export type UsersType = {
     name: string
     id: string
+    photoUrl: string
+    followed: boolean
+    status: string
+    location: LocationType
+}
+
+export type LocationType = {
+    city: string
+    country: string
 }
 
 export const usersReducer = (state: UsersInitialStateType = initialState, action: UsersActionType):UsersInitialStateType => {
